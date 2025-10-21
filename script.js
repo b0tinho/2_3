ff537fc3adbd1428a3c44f314132c2b1e017a12e
Jan Lochman <149424289+b0tinho@users.noreply.github.com>
2025-10-21 07:58:47 +0000

    Com2_2.3

--- script.js
+++ script.js
@@ -15,6 +15,7 @@
   })
 
   cw1.addEventListener("click", function() {
+    alert("Loading... ")
     answer.innerHTML = "Loading...";
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
